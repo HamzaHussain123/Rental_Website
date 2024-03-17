d2592bb72fddf4a0d84cf7b13cf26062ec7b14bf
HamzaHussain123 <hamza.hussain0504@gmail.com>
2024-03-17 15:26:37 +0500

    Fix navbar responsiveness issue

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,33 +3,40 @@ import { RxHamburgerMenu } from 'react-icons/rx'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-    const [navHeight, setnavHeight] = useState(false)
-    const navigate = useNavigate()
-    const gotoHome = () => navigate('/')
+    const [navHeight, setNavHeight] = useState(false);
+    const navigate = useNavigate();
+
+    const gotoHome = () => {
+        setNavHeight(false); // Close the navbar when navigating to Home
+        navigate('/');
+    };
+
+    const closeNav = () => {
+        setNavHeight(false); // Close the navbar when a menu item is clicked
+    };
+
     return (
         <>
             <nav className={navHeight ? "show nav" : "nav"}>
-                <div className="logo" onClick={() => gotoHome()}>HAMZA ASSOCIATES</div>
+                <div className="logo" onClick={gotoHome}>HAMZA ASSOCIATES</div>
                 <ul>
                     <li>
-                        <Link to="/home">HOME</Link>
+                        <Link to="/home" onClick={closeNav}>HOME</Link>
                     </li>
                     <li>
-                        <Link to="/aboutus">ABOUT US</Link>
+                        <Link to="/aboutus" onClick={closeNav}>ABOUT US</Link>
                     </li>
                     <li>
-                        <Link to="/villas">VILLAS</Link>
+                        <Link to="/villas" onClick={closeNav}>VILLAS</Link>
                     </li>
                     <li>
-                        <Link to="/contact">CONTACT</Link>
+                        <Link to="/contact" onClick={closeNav}>CONTACT</Link>
                     </li>
                 </ul>
-                <RxHamburgerMenu className='hamburger' onClick={() => setnavHeight(!navHeight)} />
+                <RxHamburgerMenu className='hamburger' onClick={() => setNavHeight(!navHeight)} />
             </nav>
-
         </>
-
-    )
-}
+    );
+};
 
 export default Navbar
